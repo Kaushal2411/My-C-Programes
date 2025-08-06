7e243c69dc4d2369ef1eeed1e613e722a34f02ac
Kaushal Rana <kaushal2411rana@gmail.com>
2025-08-06 21:14:20 +0530

    Create Boilerplate-code.c
    This the Basic code which is written or typed in ever C program for its execution.

---
+++ Boilerplate-code.c
@@ -0,0 +1,5 @@
+#include<stdio.h> // this line includes or bring the standard input-output library into this file 
+int main() { //from here the main code starts running
+    // the main code is to be written here
+    return 0; // this line indicates the end of the main function
+}
