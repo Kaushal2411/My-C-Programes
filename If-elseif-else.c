72bba3489892616a33ea1371c3fcb880600e66ea
Kaushal Rana <kaushal2411rana@gmail.com>
2025-08-10 20:37:14 +0530

    Update If-elseif-else.c
    Explaination

--- If-elseif-else.c
+++ If-elseif-else.c
@@ -4,25 +4,25 @@ int main() {
     int age;
     printf("Enter your age\n");
     scanf("%d", &age);
-    if (age > 60)
+    if (age > 60) // This statement will work only when the age is greater then 60.
     {
 
         printf("You can drive and you are a senior citizen");
     }
 
-    else if (age > 40)
+    else if (age > 40) // This statement will work only when the age is greater then 40.
     {
 
         printf("You can drive and you are elder");
     }
 
-    else if (age > 18)
+    else if (age > 18) // This statement will work only when the age is greater then 18.
     {
 
         printf("You can drive");
     }
 
-    else
+    else // This statement will work when all the if and elseif statements are false.
     {
 
         printf("You cannot drive");
